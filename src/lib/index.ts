01948977be990b667ca41908299eb40de1b1a718
DiegoDev <diegodev@MacBook-Pro-de-DiegoDev.local>
2024-10-07 17:33:48 -0400

    refactor

---
+++ src/lib/index.ts
@@ -0,0 +1,12 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+export let seo = {
+	title: 'LattePkg',
+	description:
+		'LattePkg is a Package Manager On Go. Beautifully designed, And Fasted In Terminal.',
+	image: '',
+	twitter: 'SEO twitter',
+	url: 'https://lattepkg.vercel.app',
+	keywords: 'svelte, sveltekit, tailwindcss, svelte animations, startup, template'
+};
+
