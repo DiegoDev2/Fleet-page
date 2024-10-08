39ac7ee0080d634afb0a7a2caf4d00ba31495c71
DiegoDev <diegodev@MacBook-Pro-de-DiegoDev.local>
2024-10-08 16:22:06 -0400

    Add Code

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -7,8 +7,8 @@
 	import { inview } from 'svelte-inview';
 
 	// Images
-	import HeroDarkImg from '$lib/imgs/hero-dark.png';
-	import HeroLightImg from '$lib/imgs/hero-light.png';
+	import HeroDarkImg from '$lib/imgs/code.png';
+	import HeroLightImg from '$lib/imgs/code.png';
 	let inView = false;
 	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
 		// console.log(detail);
@@ -55,22 +55,38 @@
 		/>
 	</Button> -->
 	<div
-		use:inview={{
-			unobserveOnEnter: true,
-			rootMargin: '-100px'
-		}}
-		on:inview_change={handleChange}
-		class="relative mt-32 animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
-	>
-		<div
-			class="rounded-xl border border-white/10 bg-[#ffebee] bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:size-full before:opacity-0 before:[background-image:linear-gradient(to_bottom,#ffccbc,#ffccbc,transparent_40%)] before:[filter:blur(180px)] {inView
-				? 'before:animate-image-glow'
-				: ''}"
-		>
-			
+  use:inview={{
+    unobserveOnEnter: true,
+    rootMargin: '-100px'
+  }}
+  on:inview_change={handleChange}
+  class="relative mt-32 animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)] shadow-[0_4px_20px_rgba((189, 195, 199);, 1)]"
+>
+  <div
+    class="rounded-xl border border-white/10 bg-[#2e2e2e] bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:size-full before:opacity-0 before:[background-image:linear-gradient(to_bottom,#a3a3a3,#a3a3a3,transparent_40%)] before:[filter:blur(180px)] {inView
+      ? 'before:animate-image-glow'
+      : ''}"
+  >
+    
+    <BorderBeam
+      size={200}
+      duration={12}
+      delay={0}
+      colorFrom="var(--color-one)"
+      colorTo="var(--color-two)"
+    />
 
-			
-			
-		</div>
-	</div>
+    <img
+      src={HeroDarkImg}
+      alt="HeroDarkImage"
+      class="relative hidden size-full rounded-[inherit] border object-contain dark:block"
+    />
+    <img
+      src={HeroLightImg}
+      alt="HeroLightImage"
+      class="relative block size-full rounded-[inherit] border object-contain dark:hidden"
+    />
+  </div>
+</div>
+
 </section>
