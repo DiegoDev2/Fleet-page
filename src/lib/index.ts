f34e7e1f2cd6418b1989ddac48fc19d0da2f02bc
DiegoDev <diegodev@MacBook-Pro-de-DiegoDev.local>
2024-10-08 16:44:00 -0400

    xd

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,7 +4,7 @@ export let seo = {
 	title: 'Fleet',
 	description:
 		'Fleet is a Package Manager On Go. Beautifully designed, And Fasted In Terminal.',
-	image: '',
+	image: 'https://repository-images.githubusercontent.com/842833730/21a2f59b-2e95-4293-8668-637f5a187790',
 	url: 'https://fleetpkg.vercel.app',
 
 };
